9a558c327154ad9e50eba946545caddf703e82c5
PandaPilot <pz513@ic.ac.uk>
2019-12-08 21:43:23 +0000

    read and write success. compiled, HITL tested

--- Heating/Receiver/Receiver.ino
+++ Heating/Receiver/Receiver.ino
@@ -1,41 +1,4 @@
-/**#include <nRF24L01.h>
-  #include <RF24.h>
-  #include <printf.h>
-  #include <RF24_config.h>
-  #include <SPI.h>
 
-  int RelayPin = 7;
-
-  RF24 radio(9, 10); // CE, CSN
-  const byte address[6] = "00002";
-  float state1[2] = {0.0, 23.4}; //on/off (1,0) and temp
-
-  void setup()
-  {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.startListening();
-  pinMode(RelayPin, OUTPUT);
-  }
-
-  void loop()
-  {
-  if (radio.available())
-  {
-    radio.read(&state1, sizeof(state1));
-    //Serial.println(state1[0]);
-    Serial.println(state1[1]);
-    digitalWrite(RelayPin, LOW);
-    delay(1000);
-  }
-  else
-  {
-    digitalWrite(RelayPin, HIGH);
-  }
-
-  }**/
 #include "nRF24L01.h" // NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
 #include "RF24.h"
 #include "SPI.h"
@@ -43,48 +6,69 @@
 typedef struct {
   int RC_no;
   float Temperature;
+  float Target;
   int Motion_no; // time count since last detection (no x 8 sec)
 } Data;
 
-Data data = {0, 0.0}; // Used to store value received by the NRF24L01
+Data data = {0, 0.0, 0.0, 0}; // Used to store value received by the NRF24L01
 String RC = "RC: ";
 String Temp = " Temperature: ";
+String Target = " Target: ";
 String out;
+
 RF24 radio(9, 10); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
 
 const byte address_1[6] = "00001"; // Needs to be the same for communicating between 2 NRF24L01
-const byte address_2[6] = "00002"; // Needs to be the same for communicating between 2 NRF24L01
-const byte address_3[6] = "00003"; // Needs to be the same for communicating between 2 NRF24L01
+//const byte address_2[6] = "00002"; // Needs to be the same for communicating between 2 NRF24L01
+//const byte address_3[6] = "00003"; // Needs to be the same for communicating between 2 NRF24L01
 const byte address_out[6] = "00011";
+
 void setup(void) {
   Serial.begin(9600);
 
   radio.begin(); // Start the NRF24L01
-
+  radio.setPALevel(RF24_PA_HIGH);
   radio.openReadingPipe(1, address_1); // Get NRF24L01 ready to receive
-  radio.openReadingPipe(2, address_2); // Get NRF24L01 ready to receive
-  radio.openReadingPipe(3, address_3); // Get NRF24L01 ready to receive
+  //radio.openReadingPipe(2, address_2); // Get NRF24L01 ready to receive
+  //radio.openReadingPipe(3, address_3); // Get NRF24L01 ready to receive
   radio.openWritingPipe(address_out);
+
   radio.startListening(); // Listen to see if information received
 
 }
 
 void loop(void) {
-
+  //radio.startListening();
   if (radio.available(address_1))
   {
     radio.read(&data, sizeof(data)); // Read information from the NRF24L01
     out = RC + data.RC_no + Temp + data.Temperature;
     Serial.println(out);
-    if (data.Motion_no==0){Serial.println("motion");}
-    else {Serial.println("away");}
-    delay(8500);
+    if (data.Motion_no == 0)
+    {
+      Serial.println("motion");
+      delay(250);
+    }
+    else if (data.Motion_no > round(16 / 8))
+    {
+      radio.stopListening();
+      radio.write(1, sizeof(1));
+      delay(250);
+      radio.startListening();
+    }
+    else
+    {
+      Serial.println("away");
+      delay(250);
+    }
+
+    delay(8250);
   }
   else
   {
     Serial.println("Not Connected");
-    data = {0, 0.0};
-    delay(8500);
+    data = {0, 0.0, 0.0, 0};
+    delay(8250);
   }
-  
+
 }
--- Heating/Transmitter/Transmitter.ino
+++ Heating/Transmitter/Transmitter.ino
@@ -1,35 +1,11 @@
+/**
+  Living Room: Radio 1 = 00001
+  Bedroom    : Radio 2 = 00002
+**/
+const int RC_number = 1;
 
-/**#include <SPI.h>
-  #include <nRF24L01.h>
-  #include <RF24.h>
+const float Target_temp = 21;
 
-  float state[2] = {0.0, 23.4}; //on/off (1,0) and temp
-  RF24 radio(7, 8); // CE, CSN
-  const byte address[6] = "00002";
-  void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.stopListening();
-  }
-  void loop() {
-
-  state[1] = state[1] + 0.1;
-  if (state[0] == 0.0)
-  {
-    state[0] = 1.0;
-  }
-  else
-  {
-    state[0] = 0.0;
-  }
-
-  radio.write(&state, sizeof(state));
-  Serial.println(state[0]);
-  Serial.print(state[1]);
-  delay(1000);
-  }**/
 
 #include "nRF24L01.h" //NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
 #include "RF24.h"
@@ -40,20 +16,26 @@
 
 #define PIR 8 // Arcade switch is connected to Pin 8 on NANO
 
-int SentMessage[1] = {000}; // Used to store value before being sent through the NRF24L01
-
 RF24 radio(9, 10); // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
 
-const byte address_out[6] = "00001";// Needs to be the same for communicating between 2 NRF24L01
 const byte address_in[6]  = "00011";
+const byte address_out[6] = "00001";// Needs to be the same for communicating between 2 NRF24L01
+
 
 typedef struct {
   int RC_no;
   float Temperature;
+  float Target;
   int Motion_no; // time count since last detection (no x 8 sec)
 } Data;
 
-Data data = {1,24.3,0};
+Data data = {RC_number, 24.3, Target_temp};
+int Return = 0;
+
+String RC = " RC: ";
+String Temp = " Temperature: ";
+String Target = " Target: ";
+String out;
 float temp = 24.5;
 bool Motion = false;
 
@@ -79,8 +61,10 @@ void setup(void) {
   Serial.begin(9600);
   radio.begin(); // Start the NRF24L01
   radio.openWritingPipe(address_out); // Get NRF24L01 ready to transmit
+
+  radio.setPALevel(RF24_PA_HIGH);
   radio.openReadingPipe(1, address_in); // Get NRF24L01 ready to receive
-  radio.startListening(); // Listen to see if information received
+  radio.stopListening(); // Listen to see if information received
   power_adc_disable(); // ADC converter
   //power_usart0_disable();// Serial (USART)
   power_timer1_disable();// Timer 1
@@ -106,21 +90,40 @@ void setup(void) {
 
 void loop(void) {
 
-  if (digitalRead(PIR) == HIGH) 
+  if (digitalRead(PIR) == HIGH)
   { // If Switch is Activated
     data.Temperature = temp;
     data.Motion_no = 0;
     //Serial.println("on");
     radio.write(&data, sizeof(data)); // Send value through NRF24L01
-    Motion=true;
+    Motion = true;
+    delay(250);
+    sleeping();
   }
-  else if(Motion) 
+  else if (Motion)
   {
     data.Temperature = temp;
-    data.Motion_no = data.Motion_no+1;
+    data.Motion_no = data.Motion_no + 1;
+    Serial.println("no motion");
     radio.write(&data, sizeof(data)); // Send value through NRF24L01
-    radio.read(&data, sizeof(data)); // Read information from the NRF24L01
+    delay(4000);
+    radio.startListening();
+    delay(2000);
+    if (radio.available(address_in))//
+    {
+      radio.read(&Return, sizeof(Return)); // Read information from the NRF24L01
+      delay(2000);
+      
+      if (Return == RC_number)
+      {
+        Motion = false;
+      }
+      Return = 0;
+    }
+    radio.stopListening();
   }
-  
-  sleeping();
+  out = Return + RC + data.RC_no + Temp + data.Temperature + Target + data.Target;
+  //Serial.println(out);
+  delay(50);
+
 }
